d4a33286e2d5f155805eec335630f5b57a8f4da4
EveLappland <fortosaber@163.com>
2024-10-06 20:39:00 +0800

    0.1

--- vllm/engine/llm_engine.py
+++ vllm/engine/llm_engine.py
@@ -1041,6 +1041,7 @@ class LLMEngine:
             # 定义批次大小
             
             batch_size = 32  # 可以根据实际情况调整
+            logger.info("max_concurrent_workers: {}".format(max_concurrent_workers))
             logger.info("Batch size: {}".format(batch_size))
             # 用于存储所有批次的结果
             ray_worker_outputs = []
