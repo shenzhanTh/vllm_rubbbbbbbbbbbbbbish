b1bbbd9193e8a02d7f970bac485231f79c90cc54
EveLappland <fortosaber@163.com>
2024-10-09 15:27:42 +0800

    cu debug

--- csrc/attention/attention_kernels.cu
+++ csrc/attention/attention_kernels.cu
@@ -15,9 +15,6 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-#ifdef USE_ROCM
-#include <hip/hip_runtime.h>
-#endif
 
 #include <torch/extension.h>
 #include <ATen/cuda/CUDAContext.h>
@@ -35,11 +32,7 @@
 // #include <cuda_runtime.h> // CUDA运行时库
 /*-----------------------------------*/
 
-#ifndef USE_ROCM
-#define WARP_SIZE 32
-#else
-#define WARP_SIZE warpSize
-#endif
+
 #define MAX(a, b) ((a) > (b) ? (a) : (b))
 #define MIN(a, b) ((a) < (b) ? (a) : (b))
 #define DIVIDE_ROUND_UP(a, b) (((a) + (b) - 1) / (b))
